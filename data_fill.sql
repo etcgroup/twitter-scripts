5b84b53fee9221f7ab0a5085921a58d738267889
Michael Brooks <mjbrooks@uw.edu>
2013-02-19 17:31:53 -0800

    fix comments in data_fill.sql

--- data_fill.sql
+++ data_fill.sql
@@ -1,22 +1,22 @@
-----
+-- --
 -- Add the sentiment column
-----
+-- --
 ALTER TABLE tweets
 ADD `sentiment` float(10,9) DEFAULT NULL;
 
 ALTER TABLE tweets
 ADD INDEX `sentiment` (`sentiment`);
 
-----
+-- --
 -- Add the tweet type fields
-----
+-- --
 ALTER TABLE tweets
 ADD `is_retweet` bool DEFAULT NULL,
 ADD `is_reply` bool DEFAULT NULL;
 
-----
+-- --
 -- Set up the is_retweet and is_reply fields
-----
+-- --
 UPDATE tweets SET
 is_retweet = (retweet_of_status_id IS NOT NULL 
 OR `text` RLIKE "^RT @.*"),
@@ -26,16 +26,16 @@ is_reply = (
     OR `text` RLIKE "^@.*"
 );
 
-----
+-- --
 -- Add indices on the reply_to fields
-----
+-- --
 ALTER TABLE tweets
 ADD INDEX `in_reply_to_status_id` (`in_reply_to_status_id`),
 ADD INDEX `in_reply_to_user_id` (`in_reply_to_user_id`);
 
-----
+-- --
 -- Add indices to the retweet and reply fields
-----
+-- --
 ALTER TABLE tweets
 ADD INDEX `is_retweet` (`is_retweet`),
 ADD INDEX `is_reply` (`is_reply`);
